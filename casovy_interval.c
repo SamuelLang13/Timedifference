d76cf92f788b087f9891ce3f40a7c030164c6fab
Samuel <langsamuel10@gmail.com>
2020-10-07 00:12:59 +0200

    Upravy kodu

--- casovy_interval.c
+++ casovy_interval.c
@@ -6,35 +6,68 @@ int main(void)
     int cas1m;
     int cas1s;
     int cas1ms;
+    int sum1;
 
     int cas2h;
     int cas2m;
     int cas2s;
     int cas2ms;
+    int sum2;
 
     printf("Zadejte cas t1:\n");
     if(scanf(" %d : %d : %d , %d",&cas1h,&cas1m,&cas1s,&cas1ms)==4)
     {
-        printf("Spravny vstup.\n");
-        return 0;
-    }
-    else
-    {
-       printf("Nespravny vstup\n");
-       return 1;
-    }
+        if(cas1h>=0 && cas1h<24 && cas1m>=0 && cas1m<60 && cas1s>=0 && cas1s<60 && cas1ms>=0 && cas1ms<1000)
+        {
+            sum1=cas1h*3600000+cas1m*60000+cas1s*1000+cas1ms;
+            printf("Spravny vstup.\n");
+            printf("Zadejte cas t2:\n");
+            if(scanf(" %d : %d : %d , %d",&cas2h,&cas2m,&cas2s,&cas2ms)==4)
+            {
+                if(cas2h>=0 && cas2h<24 && cas2m>=0 && cas2m<60 && cas2s>=0 && cas2s<60 && cas2ms>=0 && cas2ms<1000)
+                {
+                    sum2=cas2h*3600000+cas2m*60000+cas2s*1000+cas2ms;
+                    printf("Spravny vstup.\n");
+                    if(sum1>sum2)
+                    {
+                        printf("Nespravny vstup\n");
+                        return 1;
+                    }
+                    else
+                    {
+                        printf("OK\n");
+                    }
+                    
+                }
+                else
+                {
+                    printf("Nespravny vstup\n");
+                    return 1;
+                }
+            }
+            else
+            {
+                printf("Nespravny vstup\n");
+                return 1;
+            }
+               
 
-    printf("Zadejte cas t2:\n");
-    if(scanf(" %d : %d : %d , %d",&cas2h,&cas2m,&cas2s,&cas2ms)==4)
-    {
-        printf("Spravny vstup.\n");
-        return 0;
+        }
+        else
+        {
+            printf("Nespravny vstup\n");
+            return 1;
+        }
+            
     }
     else
     {
-       printf("Nespravny vstup\n");
-       return 1;
+        printf("Nespravny vstup\n");
+        return 1;
     }
+    
+
+    
 
 
 
