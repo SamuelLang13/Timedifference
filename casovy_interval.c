1141e08a6ae081d5d347c9831d07148262768b64
Samuel <langsamuel10@gmail.com>
2020-10-07 09:11:19 +0200

    Este doladit ciselny posun

--- casovy_interval.c
+++ casovy_interval.c
@@ -29,16 +29,39 @@ int main(void)
     {
         if(cas1h>=0 && cas1h<24 && cas1m>=0 && cas1m<60 && cas1s>=0 && cas1s<60 && cas1ms>=0 && cas1ms<1000)
         {
+            if(cas1ms<10)
+            {
+                cas1ms=cas1ms*100;
+                
+                sum1=cas1h*3600000+cas1m*60000+cas1s*1000+cas1ms;
+                
+            }
+            else if(cas1ms>=10)
+            {
+                sum1=cas1h*3600000+cas1m*60000+cas1s*1000+cas1ms;
+                
+            }
             
-            sum1=cas1h*3600000+cas1m*60000+cas1s*1000+cas1ms;
+
             printf("Spravny vstup.\n");
             printf("Zadejte cas t2:\n");
             if(scanf(" %d : %d : %d , %d",&cas2h,&cas2m,&cas2s,&cas2ms)==4)
             {
                 if(cas2h>=0 && cas2h<24 && cas2m>=0 && cas2m<60 && cas2s>=0 && cas2s<60 && cas2ms>=0 && cas2ms<1000)
                 {
-                    sum2=cas2h*3600000+cas2m*60000+cas2s*1000+cas2ms;
-                    printf("Spravny vstup.\n");
+                    if(cas2ms<10)
+                    {
+                        cas2ms=cas1ms*100;
+                
+                        sum1=cas2h*3600000+cas2m*60000+cas2s*1000+cas2ms;
+                
+                    }
+                    else if(cas2ms>=10)
+                    {
+                        sum2=cas2h*3600000+cas2m*60000+cas2s*1000+cas2ms;
+                
+                    }
+                    
                     if(sum1>sum2)
                     {
                         printf("Nespravny vstup\n");
@@ -46,11 +69,38 @@ int main(void)
                     }
                     else
                     {
-                        if(cas1ms<10 || cas1ms<10)
+                        printf("Spravny vstup.\n");
+
+                        diffms=cas2ms-cas1ms;
+                        diffs=cas2s-cas1s;
+                        diffm=cas2m-cas1m;
+                        diffh=cas2h-cas1h;
+
+                        if(diffms<=0)
                         {
-                            cas1ms=cas1ms*100;
-                            cas2ms=cas2ms*100;
-                        }    
+                            diffms=1000-diffms;
+                            diffs--;
+                        }
+                        else if(diffs<=0)
+                        {
+                            diffs=60-diffs;
+                            diffm--;
+                        }
+                        else if(diffm<=0)
+                        {
+                            diffm=60-diffm;
+                            diffh--;
+                        }
+
+                        totalms=diffms;
+                        totals=diffs;
+                        totalm=diffm;
+                        totalh=diffh;
+                        printf("Doba: %d:%d:%d,%d",totalh,totalm,totals,totalms);
+                        
+                        
+                        
+                        
                         
                     }
                     
